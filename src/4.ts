3aa15fb222a94e9bd719bf911b34a6c60858f7fb
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-11-24 23:57:07 +0200

    hw three typeScript

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,68 @@
+
+class Key {
+    private signature: number
+
+    constructor() {
+        this.signature =  Math.random()
+    }
+
+    getSignature(){
+      return this.signature
+    }
+
+}
+
+
+
+
+class Person{
+    constructor(private key:Key) {
+        this.key
+    }
+
+    public getKey() {
+      return this.key
+    }
+
+}
+
+
+
+abstract class House{
+    protected door: boolean
+    protected key: Key
+    protected tenants: Person[] 
+    
+    constructor(key: Key) {
+
+    this.door = false
+    this.key = key
+    this.tenants = []
+    }
+
+
+    public comeIn(person: Person) {
+        if (this.door) {   
+            this.tenants.push(person)
+        } else {
+            return
+        }
+    }
+
+
+   public abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House{
+    public openDoor(key: Key) {
+    if (this.key.getSignature() === key.getSignature()) {
+        this.door = true
+    } else {
+        this.door = false
+    }
+}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
